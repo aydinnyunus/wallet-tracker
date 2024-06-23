129e0abc0fb36834f70ffee9574bf97c8cba5c1c
Yunus AYDIN <aydinnyunus@gmail.com>
2024-06-23 13:27:13 +0300

    update docker compose binary

--- define_schema.sh
+++ define_schema.sh
@@ -1,5 +1,5 @@
 docker pull neo4j:4.4.2
-docker-compose up -d neo4j
+docker compose up -d neo4j
 # Wait till neo4j is available
 sleep 60
 
@@ -8,6 +8,6 @@ docker exec -it neo4j cypher-shell -u neo4j -p letmein -d neo4j 'CREATE CONSTRAI
 docker exec -it neo4j cypher-shell -u neo4j -p letmein -d neo4j 'CREATE CONSTRAINT IF NOT EXISTS ON (a:Address) ASSERT a.id IS UNIQUE;'
 docker exec -it neo4j cypher-shell -u neo4j -p letmein -d neo4j 'CREATE INDEX FOR (t:Transaction) ON (t.timestamp);'
 
-docker-compose stop
+docker compose stop
 
 
