f6b84e3ccdc9014b51a97ed3f2afd88d50f4d1e9
Yunus AYDIN <aydinnyunus@gmail.com>
2024-06-23 14:20:24 +0300

    update container name

--- define_schema.sh
+++ define_schema.sh
@@ -11,7 +11,7 @@ else
   exit 1
 fi
 
-docker exec -e NEO4J_USERNAME="$NEO4J_USERNAME" -e NEO4J_PASSWORD="$NEO4J_PASS" neo4j cypher-shell -u "$NEO4J_USERNAME" -p "$NEO4J_PASSWORD" "ALTER USER $NEO4J_USERNAME SET PASSWORD '$NEO4J_PASSWORD';"
+docker exec -e NEO4J_USERNAME="$NEO4J_USERNAME" -e NEO4J_PASSWORD="$NEO4J_PASS" neo4j cypher-shell -u "$NEO4J_USERNAME" -p "$NEO4J_PASS" "ALTER USER $NEO4J_USERNAME SET PASSWORD '$NEO4J_PASS';"
 
 # Define unique constraints
 docker exec neo4j cypher-shell -u $NEO4J_USERNAME -p $NEO4J_PASS -d neo4j 'CREATE CONSTRAINT IF NOT EXISTS ON (t:Transaction) ASSERT t.hash IS UNIQUE;'
